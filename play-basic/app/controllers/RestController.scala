7f136aaf6e8822a0130b85f771a969cbaebd91ea
Steffen Haase <steffen.haase@epost-dev.de>
2017-02-23 14:28:09 +0100

    bugfix

--- play-basic/app/controllers/RestController.scala
+++ play-basic/app/controllers/RestController.scala
@@ -1,33 +1,27 @@
 package controllers
 
-import controllers.RestController.{CreateMessage, Message}
+import com.google.inject.Inject
 import play.api.libs.json.{JsSuccess, Json}
 import play.api.mvc.{Action, Controller}
 
-class RestController extends Controller {
+class RestController @Inject() extends Controller {
+  case class CreateMyMessage(message: String)
+  case class MyMessage(id: Int, message: String)
+
+  implicit val createMyMessageReads = Json.reads[CreateMyMessage]
+  implicit val myMessageWrites = Json.writes[MyMessage]
+
+
   def create = Action { implicit request =>
-    request.body.asJson.map(_.validate[CreateMessage]) match {
-      case Some(JsSuccess(createMessage: CreateMessage, _)) =>
-        Created(Message(1, createMessage.message))
+    request.body.asJson.map(_.validate[CreateMyMessage]) match {
+      case Some(JsSuccess(createMessage: CreateMyMessage, _)) =>
+        Created(Json.toJson(MyMessage(1, createMessage.message)))
       case _ =>
         BadRequest
     }
   }
 
-  def get = Action {
-    Ok(Message(1, "bla"))
-  }
-}
-
-object RestController {
-  case class CreateMessage(message: String)
-  case class Message(id: Int, message: String)
-  object CreateMessage {
-    implicit val messageWrites = Json.writes[Message]
-    implicit val messageReads = Json.reads[Message]
-  }
-  object Message {
-    implicit val messageWrites = Json.writes[Message]
-    implicit val messageReads = Json.reads[Message]
+  def get(id: Int) = Action {
+    Ok(Json.toJson(MyMessage(id, "bla")))
   }
 }
